260835be71d08dc9ccef26e44c2892e929675cf7
Rodolfo Granata <warlock.cc@gmail.com>
2016-01-15 21:38:24 +0000

    * an equally functional but nicer looking tree buildir

--- src/earley/tree1.rs
+++ src/earley/tree1.rs
@@ -19,65 +19,33 @@ pub fn build_tree(grammar: &Grammar, pstate: &ParseState) -> Option<Subtree> {
     bt_helper(pstate, &root)
 }
 
-// if incomplete, lo que esta a la izquierda acumular en la lista y devolver None en el valor
-// si esta completo, stringify spec en 'valor' y meter la lista
-
 // source is always a prediction, can't be anything else cause it's on the left side
 // trigger is either a scan or a completion, only those can advance a prediction
 
 fn bt_helper(pstate: &ParseState, root: &Item) -> Option<Subtree> {
     if let Some(&(ref bp_source, ref bp_trigger)) = root.bp.iter().next() {
 
-        let source = bt_helper(pstate, bp_source);
+        let mut source = match bt_helper(pstate, bp_source) {
+            Some(n @ Subtree::Node(_, _)) => vec!(n),
+            Some(Subtree::SubT(_, childs)) => childs,
+            None =>  Vec::new()
+        };
 
         match bp_trigger {
             &Trigger::Completion(ref bp_trigger) => {
                 let trigger = bt_helper(pstate, bp_trigger);
-
-                // acumulate source
-                match source {
-                    Some(n @ Subtree::Node(_, _)) => {
-                        let mut nodes = Vec::new();
-                        nodes.push(n);
-                        if let Some(trigger) = trigger {
-                            nodes.push(trigger);
-                        }
-                        Some(Subtree::SubT(root.rule.spec(), nodes))
-                    },
-                    Some(Subtree::SubT(_, childs)) => {
-                        let mut nodes = Vec::new();
-                        nodes.extend(childs);
-                        if let Some(trigger) = trigger {
-                            nodes.push(trigger);
-                        }
-                        Some(Subtree::SubT(root.rule.spec(), nodes))
-                    },
-                    // trigger in Completion can't be None ?
-                    None => {
-                        let mut nodes = Vec::new();
-                        if let Some(trigger) = trigger {
-                            nodes.push(trigger);
-                        }
-                        Some(Subtree::SubT(root.rule.spec(), nodes))
-                    }
+                if let Some(trigger) = trigger {
+                    source.push(trigger);
                 }
+                Some(Subtree::SubT(root.rule.spec(), source))
             },
             &Trigger::Scan(ref input) => { // Eg: E -> E + . E  // source is E, trigger +
-                // acumulate source
-                match source {
-                    Some(n @ Subtree::Node(_, _)) => {
-                        let mut nodes = Vec::new();
-                        nodes.push(n);
-                        nodes.push(Subtree::Node(format!("!"), input.to_string()));
-                        Some(Subtree::SubT(format!("@"), nodes))
-                    },
-                    Some(Subtree::SubT(_, childs)) => {
-                        let mut nodes = Vec::new();
-                        nodes.extend(childs);
-                        nodes.push(Subtree::Node(format!("!"), input.to_string()));
-                        Some(Subtree::SubT(format!("@"), nodes))
-                    },
-                    None => Some(Subtree::Node(format!("#"), input.to_string()))
+
+                if source.len() == 0 { // aiming for source sub-tree to be None
+                    Some(Subtree::Node(format!("#"), input.to_string()))
+                } else {
+                    source.push(Subtree::Node(format!("!"), input.to_string()));
+                    Some(Subtree::SubT(format!("@"), source))
                 }
             }
         }

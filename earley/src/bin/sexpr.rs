8f1fc6e0bc8b9503f65be4b4241af6389c10c70a
Rodolfo Granata <warlock.cc@gmail.com>
2016-05-19 00:16:33 +0100

    evaluate

--- earley/src/bin/sexpr.rs
+++ earley/src/bin/sexpr.rs
@@ -8,6 +8,7 @@ extern crate earley;
 use earley::Subtree;
 use std::collections::HashMap;
 
+// assign   -> id '=' expr | expr
 // expr     -> expr '[+-]' addpart | addpart
 // addpart  -> addpart '[*%/]' uminus | uminus
 // uminus   -> '-' uminus | mulpart
@@ -22,7 +23,8 @@ fn build_grammar() -> earley::Grammar {
     let num = regex::Regex::new(r"^-?\d+(?:\.\d+)?(?:[eE][-+]?\d+)?$").unwrap();
     let var = regex::Regex::new(r"^[A-Za-z_]+[A-Za-z0-9_]*$").unwrap();
     let mut gb = earley::GrammarBuilder::new();
-    gb.symbol(Symbol::nonterm("expr"))
+    gb.symbol(Symbol::nonterm("assign"))
+      .symbol(Symbol::nonterm("expr"))
       .symbol(Symbol::nonterm("addpart"))
       .symbol(Symbol::nonterm("uminus"))
       .symbol(Symbol::nonterm("mulpart"))
@@ -40,8 +42,11 @@ fn build_grammar() -> earley::Grammar {
       .symbol(Symbol::terminal("[,]", |n: &str| n == ","))
       .symbol(Symbol::terminal("[(]", |n: &str| n == "("))
       .symbol(Symbol::terminal("[)]", |n: &str| n == ")"))
+      .symbol(Symbol::terminal("[=]", |n: &str| n == "="))
       ;
-    gb.rule("expr",    vec!["addpart"])
+    gb.rule("assign",  vec!["expr"])
+      .rule("assign",  vec!["[v]", "[=]", "expr"])
+      .rule("expr",    vec!["addpart"])
       .rule("expr",    vec!["expr", "[+-]", "addpart"])
       .rule("addpart", vec!["uminus"])
       .rule("addpart", vec!["addpart", "[*/%]", "uminus"])
@@ -60,7 +65,7 @@ fn build_grammar() -> earley::Grammar {
       .rule("args",    vec!["args", "[,]", "expr"])
       .rule("args",    vec![])
       ;
-    gb.into_grammar("expr")
+    gb.into_grammar("assign")
 }
 
 fn dotprinter(node: &Subtree, n: usize) {
@@ -77,60 +82,84 @@ fn dotprinter(node: &Subtree, n: usize) {
     }
 }
 
-fn seval(n: &Subtree, ctx: &mut HashMap<String, f64>) -> f64 {
+fn xeval(n: &Subtree, ctx: &mut HashMap<String, f64>) -> Vec<f64> {
     use std::str::FromStr;
     use std::f64::consts;
+
+    macro_rules! eval0 {
+        ($e:expr, $c:ident) => (xeval($e, $c)[0])
+    }
+
     match n {
         &Subtree::Node(ref key, ref val) => match key.as_ref() {
-            "[n]" => f64::from_str(&val).unwrap(),
+            "[n]" => vec![f64::from_str(&val).unwrap()],
             "[v]" => match val.as_ref() {
-                "e" => consts::E,
-                "pi" => consts::PI,
-                x => ctx[x]
+                "e" => vec![consts::E],
+                "pi" => vec![consts::PI],
+                x => vec![ctx[x]]
             },
             _ => unreachable!()
         },
         &Subtree::SubT(ref key, ref subn) => match key.as_ref() {
-            "expr -> addpart" => seval(&subn[0], ctx),
+            "assign -> expr" => xeval(&subn[0], ctx),
+            "assign -> [v] [=] expr" => {
+                let var = match &subn[0] {
+                    &Subtree::Node(_, ref var) => var.clone(),
+                    _ => unreachable!()
+                };
+                let val = xeval(&subn[2], ctx);
+                ctx.insert(var, val[0]);
+                val
+            },
+            "expr -> addpart" => xeval(&subn[0], ctx),
             "expr -> expr [+-] addpart" => match &subn[1] {
-                &Subtree::Node(_, ref op) if op == "+" => seval(&subn[0], ctx) + seval(&subn[2], ctx),
-                &Subtree::Node(_, ref op) if op == "-" => seval(&subn[0], ctx) - seval(&subn[2], ctx),
+                &Subtree::Node(_, ref op) if op == "+" => vec![eval0!(&subn[0], ctx) + eval0!(&subn[2], ctx)],
+                &Subtree::Node(_, ref op) if op == "-" => vec![eval0!(&subn[0], ctx) - eval0!(&subn[2], ctx)],
                 _ => unreachable!()
             },
-            "addpart -> uminus" => seval(&subn[0], ctx),
+            "addpart -> uminus" => xeval(&subn[0], ctx),
             "addpart -> addpart [*/%] uminus" => match &subn[1] {
-                &Subtree::Node(_, ref op) if op == "*" => seval(&subn[0], ctx) * seval(&subn[2], ctx),
-                &Subtree::Node(_, ref op) if op == "-" => seval(&subn[0], ctx) / seval(&subn[2], ctx),
-                &Subtree::Node(_, ref op) if op == "%" => seval(&subn[0], ctx) % seval(&subn[2], ctx),
+                &Subtree::Node(_, ref op) if op == "*" => vec![eval0!(&subn[0], ctx) * eval0!(&subn[2], ctx)],
+                &Subtree::Node(_, ref op) if op == "/" => vec![eval0!(&subn[0], ctx) / eval0!(&subn[2], ctx)],
+                &Subtree::Node(_, ref op) if op == "%" => vec![eval0!(&subn[0], ctx) % eval0!(&subn[2], ctx)],
                 _ => unreachable!()
             },
-            "uminus -> mulpart" => seval(&subn[0], ctx),
-            "uminus -> [-] uminus" => - seval(&subn[1], ctx),
-            "mulpart -> ufact" => seval(&subn[0], ctx),
+            "uminus -> mulpart" => xeval(&subn[0], ctx),
+            "uminus -> [-] uminus" => vec![- eval0!(&subn[1], ctx)],
+            "mulpart -> ufact" => xeval(&subn[0], ctx),
             "mulpart -> ufact [^] uminus" => match &subn[1] {
-                &Subtree::Node(_, ref op) if op == "^" => seval(&subn[0], ctx).powf(seval(&subn[2], ctx)),
+                &Subtree::Node(_, ref op) if op == "^" => vec![eval0!(&subn[0], ctx).powf(eval0!(&subn[2], ctx))],
                 _ => unreachable!()
             },
-            "ufact -> group" => seval(&subn[0], ctx),
+            "ufact -> group" => xeval(&subn[0], ctx),
             "ufact -> ufact [!]" => panic!(), // no gamma function?
-            "group -> [n]" => seval(&subn[0], ctx),
-            "group -> [v]" => seval(&subn[0], ctx),
-            "group -> [(] expr [)]" => seval(&subn[1], ctx),
-            "group -> func" => seval(&subn[0], ctx),
-            "func -> [v] [(] args [)]" => match &subn[0] {
-                &Subtree::Node(_, ref f) if f == "sin" => seval(&subn[2], ctx).sin(),
-                &Subtree::Node(_, ref f) if f == "cos" => seval(&subn[2], ctx).cos(),
-                _ => panic!()
+            "group -> [n]" => xeval(&subn[0], ctx),
+            "group -> [v]" => xeval(&subn[0], ctx),
+            "group -> [(] expr [)]" => xeval(&subn[1], ctx),
+            "group -> func" => xeval(&subn[0], ctx),
+            "func -> [v] [(] args [)]" => {
+                let args = xeval(&subn[2], ctx);
+                match &subn[0] {
+                    &Subtree::Node(_, ref f) if f == "sin" => vec![args[0].sin()],
+                    &Subtree::Node(_, ref f) if f == "cos" => vec![args[0].cos()],
+                    &Subtree::Node(_, ref f) if f == "max" => match args.len() {
+                        0 => vec![],
+                        _ => vec![args.iter().cloned().fold(std::f64::NAN, f64::max)]
+                    },
+                    _ => panic!()
+                }
             },
-            "args -> expr" => seval(&subn[0], ctx),
-            "args -> args [,] expr" => panic!(), // TODO: flatten args
-            "args ->" => panic!(), // ????
+            "args -> expr" => xeval(&subn[0], ctx),
+            "args -> args [,] expr" => {
+                let mut a = xeval(&subn[0], ctx);
+                a.push(eval0!(&subn[2], ctx));
+                a
+            }
+            "args ->" => vec![],
             _ => unreachable!()
         }
     }
 }
-
-// TODO: build AST from Subtree
 
 struct Tokenizer(lexers::Scanner<char>);
 
@@ -168,13 +197,15 @@ fn main() {
         return;
     }
 
+    let mut ctx = HashMap::new();
     while let Some(input) = linenoise::input("~> ") {
         linenoise::history_add(&input[..]);
         match parser.parse(&mut Tokenizer::from_str(&input)) {
             Ok(estate) => {
                 let tree = earley::one_tree(parser.g.start(), &estate);
-                let mut ctx = HashMap::new();
-                println!("{:?}", seval(&tree, &mut ctx));
+                let val = xeval(&tree, &mut ctx)[0];
+                ctx.insert(format!["ans"], val);
+                println!("{:?}", val);
             },
             Err(e) => println!("Parse err: {:?}", e)
         }

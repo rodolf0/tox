f2abb2db573d4955de9d51007f0a90f00e04081b
Rodolfo Granata <warlock.cc@gmail.com>
2016-08-29 20:12:16 +0100

    test constants

--- kronos/src/constants.rs
+++ kronos/src/constants.rs
@@ -1,7 +1,3 @@
-use std::collections::HashMap;
-
-use std::iter::FromIterator;
-
 fn weekday(d: &str) -> Option<usize> {
     match d {
         "sunday"    |  "sun" => Some(0),
@@ -17,18 +13,18 @@ fn weekday(d: &str) -> Option<usize> {
 
 fn month(m: &str) -> Option<usize> {
     match m {
-        "january"   |  "jan" => Some(1),
-        "february"  |  "feb" => Some(2),
-        "march"     |  "mar" => Some(3),
-        "april"     |  "apr" => Some(4),
-        "may"       |  "may" => Some(5),
-        "june"      |  "jun" => Some(6),
-        "july"      |  "jul" => Some(7),
-        "august"    |  "aug" => Some(8),
-        "september" |  "sep" => Some(9),
-        "october"   |  "oct" => Some(10),
-        "november"  |  "nov" => Some(11),
-        "december"  |  "dec" => Some(12),
+        "january"   |  "jan"    => Some(1),
+        "february"  |  "feb"    => Some(2),
+        "march"     |  "mar"    => Some(3),
+        "april"     |  "apr"    => Some(4),
+        "may"       => Some(5),
+        "june"      |  "jun"    => Some(6),
+        "july"      |  "jul"    => Some(7),
+        "august"    |  "aug"    => Some(8),
+        "september" |  "sep"    => Some(9),
+        "october"   |  "oct"    => Some(10),
+        "november"  |  "nov"    => Some(11),
+        "december"  |  "dec"    => Some(12),
         _           => None
     }
 }
@@ -43,17 +39,33 @@ fn ordinal(n: &str) -> Option<usize> {
         "twenty-seventh", "twenty-eigth", "twenty-ninth", "thirtieth",
         "thirty-first",
     ];
-    let ord = ORD.iter()
+    ORD.iter()
        .enumerate()
-       .filter_map(|(i, name)| match *name == n { true => Some(i), _=> None })
-       .next();
+       .filter_map(|(i, name)| match *name == n { true => Some(i+1), _=> None })
+       .next()
 }
 
 fn short_ordinal(n: &str) -> Option<usize> {
     use std::str::FromStr;
     let num = n.chars().take_while(|d| d.is_numeric()).collect::<String>();
-    match &n[d.len()..] {
-        "st"|"nd"|"rd"|"th" => usize::from_str(num),
+    match &n[num.len()..] {
+        "st"|"nd"|"rd"|"th" => usize::from_str(&num).ok(),
         _ => None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{ordinal, short_ordinal};
+    #[test]
+    fn test_short_ordinal() {
+        assert_eq!(short_ordinal("22nd"), Some(22));
+        assert_eq!(short_ordinal("43rd"), Some(43));
+        assert_eq!(short_ordinal("5ht"), None);
+    }
+    #[test]
+    fn test_ordinal() {
+        assert_eq!(ordinal("twenty-fourth"), Some(24));
+        assert_eq!(ordinal("twelveth"), Some(12));
     }
 }

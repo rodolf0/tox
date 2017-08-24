b730f37d0d8513671172d270fd89a56f8cea3d81
Rodolfo Granata <warlock.cc@gmail.com>
2017-08-24 09:16:50 -0400

    cleanup arith example

--- earlgrey/examples/arith.rs
+++ earlgrey/examples/arith.rs
@@ -6,7 +6,6 @@ use std::cell::RefCell;
 fn build_grammar() -> earlgrey::Grammar {
     use std::str::FromStr;;
     earlgrey::GrammarBuilder::new()
-      .symbol("assign")
       .symbol("expr")
       .symbol("term")
       .symbol("factor")
@@ -16,7 +15,6 @@ fn build_grammar() -> earlgrey::Grammar {
       .symbol("func")
       .symbol("args")
       .symbol(("[n]", |n: &str| f64::from_str(n).is_ok()))
-      .symbol(("[v]", |n: &str| n.chars().all(|c| c.is_alphabetic() || c == '_')))
       .symbol(("+", |n: &str| n == "+"))
       .symbol(("-", |n: &str| n == "-"))
       .symbol(("*", |n: &str| n == "*"))
@@ -28,8 +26,6 @@ fn build_grammar() -> earlgrey::Grammar {
       .symbol(("(", |n: &str| n == "("))
       .symbol((")", |n: &str| n == ")"))
       .symbol(("=", |n: &str| n == "="))
-      .rule("assign", &["expr"])
-      //.rule("assign", &["[v]", "=", "expr"])
       .rule("expr",   &["term"])
       .rule("expr",   &["expr", "+", "term"])
       .rule("expr",   &["expr", "-", "term"])
@@ -44,14 +40,8 @@ fn build_grammar() -> earlgrey::Grammar {
       .rule("ufact",  &["group"])
       .rule("ufact",  &["ufact", "!"])
       .rule("group",  &["[n]"])
-      //.rule("group",  &["[v]"])
       .rule("group",  &["(", "expr", ")"])
-      //.rule("group",  &["func"])
-      //.rule("func",   &["[v]", "(", "args", ")"])
-      //.rule("args",   &["expr"])
-      //.rule("args",   &["args", ",", "expr"])
-      //.rule::<_, &str>("args",   &[])
-      .into_grammar("assign")
+      .into_grammar("expr")
       .expect("Bad Gramar")
 }
 
@@ -85,8 +75,6 @@ fn semanter<'a>() -> earlgrey::EarleyEvaler<'a, f64> {
     let mut ev = earlgrey::EarleyEvaler::new(|symbol, token| {
         match symbol {"[n]" => f64::from_str(token).unwrap(), _ => 0.0}
     });
-    ev.action("assign -> expr", |n| n[0]);
-    //ev.action("assign -> [v] = expr", || {});
     ev.action("expr -> term", |n| n[0]);
     ev.action("expr -> expr + term", |n| n[0] + n[2]);
     ev.action("expr -> expr - term", |n| n[0] - n[2]);
@@ -101,13 +89,7 @@ fn semanter<'a>() -> earlgrey::EarleyEvaler<'a, f64> {
     ev.action("ufact -> group", |n| n[0]);
     ev.action("ufact -> ufact !", |n| gamma(n[0]+1.0));
     ev.action("group -> [n]", |n| n[0]);
-    //ev.action("group -> [v]", |n| {});
     ev.action("group -> ( expr )", |n| n[1]);
-    //ev.action("group -> func", || {});
-    //ev.action("func -> [v] ( args )", || {});
-    //ev.action("args -> expr", || {});
-    //ev.action("args -> args , expr", || {});
-    //ev.action("args -> ", || {});
     ev
 }
 

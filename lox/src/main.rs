b06724c1f5768f429de927f4075cd4f1ad239362
Rodolfo Granata <warlock.cc@gmail.com>
2017-11-18 18:54:41 -0500

    lox: keep interpreter around in interactive mode

--- lox/src/main.rs
+++ lox/src/main.rs
@@ -23,13 +23,12 @@ fn main() {
         return;
     }
 
-    let run = |source: String| {
+    let run = |source: String, interpreter: &mut LoxInterpreter| {
         let scanner = LoxScanner::scanner(source);
         let mut parser = LoxParser::new(scanner);
-        let mut interpreter = LoxInterpreter::new();
         match parser.parse() {
             Ok(stmts) => {
-                match Resolver::new(&mut interpreter).resolve(&stmts) {
+                match Resolver::new(interpreter).resolve(&stmts) {
                     Ok(_) => if let Err(error) = interpreter.interpret(&stmts) {
                         eprintln!("LoxInterpreter error: {}", error)
                     },
@@ -40,12 +39,13 @@ fn main() {
         }
     };
 
+    let mut interpreter = LoxInterpreter::new();
     if env::args().len() == 2 {
         let sourcefile = env::args().skip(1).next().unwrap();
         if let Ok(mut f) = File::open(&sourcefile) {
             let mut source = String::new();
             if f.read_to_string(&mut source).is_ok() {
-                return run(source);
+                return run(source, &mut interpreter);
             }
         }
         eprintln!("lox: failed to read source file {}", sourcefile);
@@ -56,7 +56,7 @@ fn main() {
             io::stdout().write(b"~> ").unwrap();
             io::stdout().flush().unwrap();
             match io::stdin().read_line(&mut input) {
-                Ok(_) => run(input),
+                Ok(_) => run(input, &mut interpreter),
                 Err(e) => eprintln!("lox read_line error: {:?}", e)
             }
         }

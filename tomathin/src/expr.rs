fd4ec8aa8c58224c6def90c9954e5b933f4d8bd9
Rodolfo Granata <warlock.cc@gmail.com>
2024-12-10 00:06:08 -0500

    Tomathin: fixup Times over List to reduce numerics

--- tomathin/src/expr.rs
+++ tomathin/src/expr.rs
@@ -88,6 +88,26 @@ fn distribute_op(lhs: Expr, rhs: Expr, op: &str, over: &str) -> Expr {
     }
 }
 
+fn flatten(expr: Expr, op: &str) -> Expr {
+    match expr {
+        Expr::Expr(head, args) if head == op => Expr::Expr(
+            head,
+            args.into_iter()
+                .flat_map(|ai| match ai {
+                    Expr::Expr(h, a) if h == op => {
+                        a.into_iter().map(|aj| flatten(aj, op)).collect()
+                    }
+                    other => vec![flatten(other, op)],
+                })
+                .collect(),
+        ),
+        Expr::Expr(head, args) => {
+            Expr::Expr(head, args.into_iter().map(|ai| flatten(ai, op)).collect())
+        }
+        other => other,
+    }
+}
+
 pub fn evaluate(expr: Expr) -> Result<Expr, String> {
     eval_with_ctx(expr, &mut Context::new())
 }
@@ -156,30 +176,41 @@ pub fn eval_with_ctx(expr: Expr, ctx: &mut Context) -> Result<Expr, String> {
                 // 3 * x * {a, b} => {3 * x * a, 3 * x * b}
                 // 3 * {4, 5} => {12, 15}
 
+                // Distribute Times over List
                 let mut new_args = vec![args.remove(0)];
                 for arg in args {
+                    let rhs = eval_with_ctx(arg, ctx)?;
+                    new_args = new_args
+                        .into_iter()
+                        .map(|lhsi| {
+                            flatten(distribute_op(lhsi, rhs.clone(), "Times", "List"), "Times")
+                        })
+                        .collect();
+                }
+                // Peel off wrapping Times result of distributing Times over List to avoid inf recurse
+                new_args = new_args
+                    .into_iter()
+                    .flat_map(|expr| match expr {
+                        Expr::Expr(h, a) if h == "Times" => a,
+                        o => vec![o],
+                    })
+                    .collect();
+                // Run mmultiplication
+                let mut numeric: Option<f64> = None;
+                let mut new_args2 = Vec::new();
+                for arg in new_args {
                     match eval_with_ctx(arg, ctx)? {
-                        rhs @ Expr::Number(rhsn) => {
-                            new_args = new_args
-                                .into_iter()
-                                .map(|argi| match argi {
-                                    Expr::Number(lhs) => Expr::Number(lhs * rhsn),
-                                    lhs => distribute_op(lhs, rhs.clone(), "Times", "List"),
-                                })
-                                .collect();
-                        }
-                        rhs => {
-                            new_args = new_args
-                                .into_iter()
-                                .map(|lhsi| distribute_op(lhsi, rhs.clone(), "Times", "List"))
-                                .collect();
-                        }
+                        Expr::Number(n) => *numeric.get_or_insert(1.0) *= n,
+                        o => new_args2.push(o),
                     }
                 }
-                if new_args.len() == 1 {
-                    Ok(new_args.swap_remove(0))
+                if numeric.is_some_and(|n| n != 1.0) || new_args2.len() == 0 {
+                    new_args2.insert(0, Expr::Number(numeric.unwrap()));
+                }
+                if new_args2.len() == 1 {
+                    Ok(new_args2.swap_remove(0))
                 } else {
-                    Ok(Expr::Expr(head, new_args))
+                    Ok(Expr::Expr(head, new_args2))
                 }
             }
             "Minus" | "Power" | "Divide" => {

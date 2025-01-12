17b6b8bdc9c36f13c7c637bc54689b5491250240
Rodolfo Granata <warlock.cc@gmail.com>
2025-01-12 03:02:16 -0500

    rust cleanup

--- lox/src/lox_resolver.rs
+++ lox/src/lox_resolver.rs
@@ -1,7 +1,7 @@
 #![deny(warnings)]
 
-use crate::lox_parser::{Expr, Stmt};
 use crate::lox_interpreter::LoxInterpreter;
+use crate::lox_parser::{Expr, Stmt};
 use std::collections::HashMap;
 
 type ResolveResult = Result<(), String>;
@@ -13,27 +13,37 @@ pub struct Resolver<'a> {
 }
 
 impl<'a> Resolver<'a> {
-    pub fn new(interp: &'a mut LoxInterpreter) -> Resolver {
-        Resolver{interpreter: interp, scopes: Vec::new()}
+    pub fn new(interp: &mut LoxInterpreter) -> Resolver {
+        Resolver {
+            interpreter: interp,
+            scopes: Vec::new(),
+        }
     }
 
     pub fn resolve(&mut self, stmts: &[Stmt]) -> ResolveResult {
-        stmts.iter().map(|stmt| self.resolve_stmt(stmt))
-             .find(|stmt| stmt.is_err())
-             .unwrap_or(Ok(()))
+        stmts
+            .iter()
+            .map(|stmt| self.resolve_stmt(stmt))
+            .find(|stmt| stmt.is_err())
+            .unwrap_or(Ok(()))
     }
 
-    fn begin_scope(&mut self) { self.scopes.push(HashMap::new()); }
+    fn begin_scope(&mut self) {
+        self.scopes.push(HashMap::new());
+    }
 
-    fn end_scope(&mut self) { self.scopes.pop(); }
+    fn end_scope(&mut self) {
+        self.scopes.pop();
+    }
 
     fn declare(&mut self, token: String) -> ResolveResult {
         use std::collections::hash_map::Entry::*;
         if let Some(scope) = self.scopes.last_mut() {
             match scope.entry(token.clone()) {
-                Occupied(_) => return
-                    Err(format!("Var {} already declared in scope", token)),
-                Vacant(spot) => { spot.insert(false); }
+                Occupied(_) => return Err(format!("Var {} already declared in scope", token)),
+                Vacant(spot) => {
+                    spot.insert(false);
+                }
             }
         }
         Ok(())
@@ -48,7 +58,10 @@ impl<'a> Resolver<'a> {
 
     fn resolve_local(&mut self, expr: &Expr, name: &str) -> ResolveResult {
         // find the scope that contains the name
-        let scope = self.scopes.iter().rev()
+        let scope = self
+            .scopes
+            .iter()
+            .rev()
             .enumerate()
             .find(|&(_, scope)| scope.contains_key(name));
         // bind the interpreter's reference to that scope
@@ -58,8 +71,7 @@ impl<'a> Resolver<'a> {
         Ok(())
     }
 
-    fn resolve_function(&mut self, params: &[String],
-                        body: &[Stmt]) -> ResolveResult {
+    fn resolve_function(&mut self, params: &[String], body: &[Stmt]) -> ResolveResult {
         self.begin_scope();
         for param in params {
             self.declare(param.clone())?;
@@ -75,38 +87,37 @@ impl<'a> Resolver<'a> {
             &Expr::Logical(ref left, _, ref right) => {
                 self.resolve_expr(left)?;
                 self.resolve_expr(right)
-            },
+            }
             &Expr::Binary(ref left, _, ref right) => {
                 self.resolve_expr(left)?;
                 self.resolve_expr(right)
-            },
+            }
             &Expr::Unary(_, ref unary) => self.resolve_expr(unary),
-            &Expr::Nil | &Expr::Bool(_) |
-            &Expr::Num(_) | &Expr::Str(_) => Ok(()),
+            &Expr::Nil | &Expr::Bool(_) | &Expr::Num(_) | &Expr::Str(_) => Ok(()),
             &Expr::Grouping(ref gexpr) => self.resolve_expr(gexpr),
             &Expr::Var(ref token) => {
                 if let Some(scope) = self.scopes.last() {
                     if scope.get(&token.lexeme) == Some(&false) {
-                        return Err(format!(
-                            "Can't read var in initializer {:?}", token));
+                        return Err(format!("Can't read var in initializer {:?}", token));
                     }
                 }
                 self.resolve_local(expr, &token.lexeme)
-            },
+            }
             &Expr::Assign(ref token, ref asigex) => {
                 self.resolve_expr(asigex)?;
                 self.resolve_local(expr, &token.lexeme)
-            },
+            }
             &Expr::Call(ref callee, ref args) => {
                 self.resolve_expr(callee)?;
-                args.iter().map(|arg| self.resolve_expr(arg))
+                args.iter()
+                    .map(|arg| self.resolve_expr(arg))
                     .find(|arg| arg.is_err())
                     .unwrap_or(Ok(()))
-            },
+            }
         }
     }
 
-    fn resolve_stmt(&mut self, stmt: &Stmt)  -> ResolveResult {
+    fn resolve_stmt(&mut self, stmt: &Stmt) -> ResolveResult {
         match stmt {
             Stmt::Print(ref expr) => self.resolve_expr(expr),
             Stmt::Expr(ref expr) => self.resolve_expr(expr),
@@ -118,13 +129,13 @@ impl<'a> Resolver<'a> {
                     _ => self.resolve_expr(init)?,
                 }
                 self.define(name.clone())
-            },
+            }
             Stmt::Block(ref stmts) => {
                 self.begin_scope();
                 self.resolve(stmts)?;
                 self.end_scope();
                 Ok(())
-            },
+            }
             Stmt::If(ref cond, ref then, ref elseb) => {
                 self.resolve_expr(cond)?;
                 self.resolve_stmt(then)?;
@@ -132,17 +143,17 @@ impl<'a> Resolver<'a> {
                     self.resolve_stmt(elseb)?;
                 }
                 Ok(())
-            },
+            }
             Stmt::While(ref cond, ref body) => {
                 self.resolve_expr(cond)?;
                 self.resolve_stmt(body)
-            },
+            }
             Stmt::Break(_) => Ok(()),
             Stmt::Function(ref name, ref parameters, ref body) => {
                 self.declare(name.clone())?;
                 self.define(name.clone())?;
                 self.resolve_function(parameters, body)
-            },
+            }
             Stmt::Return(ref expr) => self.resolve_expr(expr),
         }
     }

1906d1f9f84a29a0014a09b7bae2e925cb6b0fea
Rodolfo Granata <warlock.cc@gmail.com>
2025-03-13 15:10:47 -0400

    numerica: move replace_all to its own module

---
+++ numerica/src/expr/replace_all.rs
@@ -0,0 +1,91 @@
+use super::{eval_with_ctx, Expr};
+use crate::context::Context;
+
+pub fn eval_replace_all(expr: Expr, ctx: &mut Context) -> Result<Expr, String> {
+    // Eval of replace_all is outside of eval because evaluation of expr is deferred
+    match expr {
+        Expr::Expr(head, args) if head == "ReplaceAll" => {
+            let [expr, rules]: [Expr; 2] = args
+                .try_into()
+                .map_err(|e| format!("ReplaceAll must have 2 arguments. {:?}", e))?;
+            eval_with_ctx(
+                replace_all(
+                    // Nested evaluation of replace_all without eval of expr
+                    eval_replace_all(expr, ctx)?,
+                    eval_rules(rules, ctx)?.as_slice(),
+                )?,
+                ctx,
+            )
+        }
+        other => Ok(other),
+    }
+}
+
+// ReplaceAll[x, Rule[x, 3]]
+// ReplaceAll[List[1, 2, 3], Rule[List, FindRoot]]
+pub fn replace_all(expr: Expr, rules: &[(Expr, Expr)]) -> Result<Expr, String> {
+    match expr {
+        // for each sub-expression apply the replacement
+        Expr::Expr(head, args) => {
+            // First execute replace_all on subexpressions.
+            let replaced_expr = Expr::Expr(
+                head,
+                args.into_iter()
+                    .map(|a| replace_all(a, rules))
+                    .collect::<Result<_, _>>()?,
+            );
+            // Check if update expression matches a replacement too.
+            let replaced_expr = rules
+                .iter()
+                .filter(|(lhs, _)| replaced_expr == *lhs)
+                .next()
+                .map(|(_, rhs)| rhs.clone())
+                .unwrap_or(replaced_expr);
+            // Check if a rule is a head re-write
+            Ok(match replaced_expr {
+                Expr::Expr(head, args) => {
+                    for r in rules {
+                        if let (Expr::Symbol(lhs), Expr::Symbol(rhs)) = r {
+                            if *lhs == head {
+                                return Ok(Expr::Expr(rhs.clone(), args));
+                            }
+                        }
+                    }
+                    Expr::Expr(head, args)
+                }
+                expr => expr,
+            })
+        }
+        atom => Ok(rules
+            .iter()
+            .filter(|(lhs, _)| atom == *lhs)
+            .next()
+            .map(|(_, rhs)| rhs.clone())
+            .unwrap_or(atom)),
+    }
+}
+
+fn eval_rules(rules: Expr, ctx: &mut Context) -> Result<Vec<(Expr, Expr)>, String> {
+    // eval_with_ctx rules and check they result in Rule or List[Rule]
+    match eval_with_ctx(rules, ctx)? {
+        Expr::Expr(head, args) if head == "Rule" => {
+            let [lhs, rhs]: [Expr; 2] = args
+                .try_into()
+                .map_err(|e| format!("Rule must have 2 arguments. {:?}", e))?;
+            Ok(vec![(lhs, rhs)])
+        }
+        Expr::Expr(h, args) if h == "List" => args
+            .into_iter()
+            .map(|rule| match rule {
+                Expr::Expr(head, args) if head == "Rule" => {
+                    let [lhs, rhs]: [Expr; 2] = args
+                        .try_into()
+                        .map_err(|e| format!("Rule must have 2 arguments. {:?}", e))?;
+                    Ok((lhs, rhs))
+                }
+                other => Err(format!("Expected all args to be Rule: {:?}", other)),
+            })
+            .collect(),
+        other => Err(format!("ReplaceAll unexpected arg: '{:?}'", other)),
+    }
+}

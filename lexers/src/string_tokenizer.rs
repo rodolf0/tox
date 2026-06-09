1cfc48e80b3d46ca886f39d17d341806fb9b27bd
Rodolfo Granata <warlock.cc@gmail.com>
2026-06-09 00:24:39 -0400

    tmp: building towards a more flexible string_tokenizer

--- lexers/src/string_tokenizer.rs
+++ lexers/src/string_tokenizer.rs
@@ -1,94 +1,166 @@
 use crate::scanner::{Scan, Scanner};
 
-#[derive(Clone)]
-pub struct EscapePair {
-    start: String,
-    end: String,
-    escape_char: Option<char>,
-    keep_delimiters: bool,
+trait Extractor2<I: Iterator<Item = char>> {
+    fn extract<'a>(&self, scanner: &'a mut Scanner<I>) -> Option<&'a [char]>;
 }
 
-impl EscapePair {
-    pub fn new(start: impl AsRef<str>, end: impl AsRef<str>) -> Self {
-        Self {
-            start: start.as_ref().to_string(),
-            end: end.as_ref().to_string(),
-            escape_char: Some('\\'),
-            keep_delimiters: true,
+impl<I: Iterator<Item = char>, const N: usize> Extractor2<I> for [&str; N] {
+    fn extract<'a>(&self, scanner: &'a mut Scanner<I>) -> Option<&'a [char]> {
+        for splitter in *self {
+            if scanner.accept_seq(splitter.chars()) {
+                return Some(scanner.view());
+            }
+        }
+        None
+    }
+}
+
+impl<I: Iterator<Item = char>, Func> Extractor2<I> for Func
+where
+    Func: Fn(&mut Scanner<I>) -> Option<&[char]>,
+{
+    fn extract<'a>(&self, scanner: &'a mut Scanner<I>) -> Option<&'a [char]> {
+        self(scanner)
+    }
+}
+
+pub fn symbols<I: Iterator<Item = char>>(
+    syms: impl IntoIterator<Item = impl AsRef<str>>,
+) -> impl Fn(&mut Scanner<I>) -> Option<&[char]> {
+    let syms = syms
+        .into_iter()
+        .map(|s| s.as_ref().to_string())
+        .collect::<Vec<_>>();
+    move |scanner: &mut Scanner<I>| {
+        for splitter in &syms {
+            if scanner.accept_seq(splitter.chars()) {
+                return Some(scanner.view());
+            }
+        }
+        None
+    }
+}
+
+pub fn quoted<I: Iterator<Item = char>>(
+    start: &str,
+    end: &str,
+    escape: Option<char>,
+) -> impl Fn(&mut Scanner<I>) -> Option<&[char]> {
+    move |scanner: &mut Scanner<I>| {
+        if extract_quoted(scanner, start, end, escape).is_some() {
+            return Some(scanner.view());
+        }
+        None
+    }
+}
+
+// TODO: move this to extractors.rs
+pub fn extract_quoted<'a, I: Iterator<Item = char>>(
+    s: &'a mut Scanner<I>,
+    start: &str,
+    end: &str,
+    escape: Option<char>,
+) -> Option<&'a [char]> {
+    let cp = s.checkpoint();
+    if s.accept_seq(start.chars()) {
+        // consume all escaped content until end delimiter.
+        while let Some(c) = s.peek() {
+            if Some(*c) == escape {
+                s.advance(); // consume escape char
+            } else if s.accept_seq(end.chars()) {
+                return Some(s.view());
+            }
+            s.advance();
         }
     }
-
-    pub fn escape_char(mut self, escape_char: Option<char>) -> Self {
-        self.escape_char = escape_char;
-        self
-    }
-
-    pub fn drop_delimiters(mut self) -> Self {
-        self.keep_delimiters = false;
-        self
-    }
+    s.restore(cp);
+    None
+    //
+    // // strip delimiters if needed
+    // let result: String = self.src.lift().collect();
+    // if !pair.keep_delimiters {
+    //     let mut s = result.as_str();
+    //     s = s.strip_prefix(&pair.start).unwrap_or(s);
+    //     s = s.strip_suffix(&pair.end).unwrap_or(s);
+    //     return Some(s.to_string());
+    // }
+    // return Some(result);
 }
 
-#[derive(Clone)]
-struct Splitter {
-    text: String,
-    is_delimiter: bool, // delimiters are dropped, symbols returned.
+struct Extractor<'a, I: Iterator<Item = char>> {
+    extractor: Box<dyn Fn(&mut Scanner<I>) -> Option<&[char]> + 'a>,
+    discard: bool, // should this match be discarded or returned.
 }
 
-pub struct StringTokenizer<I: Iterator<Item = char>> {
+// pub struct StringTokenizer<I: Iterator<Item = char>> {
+pub struct StringTokenizer<'a, I: Iterator<Item = char>> {
     src: Scanner<I>,
-    splitters: Vec<Splitter>,
-    escape_pairs: Vec<EscapePair>,
     // Configures what chars are discarded while scanning. Default: whitespace.
     trimmer: fn(&char) -> bool,
+    // Extractors act like complex splitters.
+    // They can match a splitter by using the scanner.
+    // On no match they need to leave the scanner in its original state.
+    // escape pairs are a form of extractor.
+    extractors: Vec<Extractor<'a, I>>,
+    extractors2: Vec<(Box<dyn Extractor2<I> + 'a>, bool)>,
+    queued_token: Option<String>,
 }
 
-impl<'a> From<&'a str> for StringTokenizer<std::str::Chars<'a>> {
+// impl<'a> From<&'a str> for StringTokenizer<std::str::Chars<'a>> {
+impl<'a> From<&'a str> for StringTokenizer<'a, std::str::Chars<'a>> {
     fn from(s: &'a str) -> Self {
         Self::new(s.chars())
     }
 }
 
-impl<I: Iterator<Item = char>> StringTokenizer<I> {
+// impl<I: Iterator<Item = char>> StringTokenizer<I> {
+impl<'a, I: Iterator<Item = char>> StringTokenizer<'a, I> {
     pub fn new(source: I) -> Self {
         StringTokenizer {
             src: source.scanner(),
-            splitters: Vec::new(),
-            escape_pairs: Vec::new(),
             trimmer: |c: &char| c.is_whitespace(),
+            extractors: Vec::new(),
+            extractors2: Vec::new(),
+            queued_token: None,
         }
     }
 
-    pub fn escape_pairs(mut self, pairs: impl IntoIterator<Item = EscapePair>) -> Self {
-        for p in pairs {
-            self.escape_pairs.push(p);
-        }
-        self.escape_pairs
-            .sort_by_key(|p| std::cmp::Reverse(p.start.len()));
+    pub fn split_on<S>(mut self, s: S, discard: bool) -> Self
+    where
+        S: Fn(&mut Scanner<I>) -> Option<&[char]> + 'a,
+    {
+        self.extractors.push(Extractor {
+            extractor: Box::new(s),
+            discard,
+        });
         self
     }
 
-    pub fn symbols(mut self, syms: impl IntoIterator<Item = impl AsRef<str>>) -> Self {
-        for s in syms {
-            self.splitters.push(Splitter {
-                text: s.as_ref().to_string(),
-                is_delimiter: false,
-            });
-        }
-        self.splitters
-            .sort_by_key(|b| std::cmp::Reverse(b.text.len()));
+    pub fn split_on2(mut self, s: impl Extractor2<I> + 'a, discard: bool) -> Self {
+        self.extractors2.push((Box::new(s), discard));
         self
     }
 
-    pub fn delimiters(mut self, delims: impl IntoIterator<Item = impl AsRef<str>>) -> Self {
-        for s in delims {
-            self.splitters.push(Splitter {
-                text: s.as_ref().to_string(),
-                is_delimiter: true,
+    pub fn extractor<E>(mut self, e: E, discard: bool) -> Self
+    where
+        E: Fn(&mut Scanner<I>) -> Option<&[char]> + 'a,
+    {
+        self.extractors.push(Extractor {
+            extractor: Box::new(e),
+            discard,
+        });
+        self
+    }
+
+    pub fn delimiters(mut self, delims: impl IntoIterator<Item = impl AsRef<str> + 'a>) -> Self {
+        for splitter in delims {
+            self.extractors.push(Extractor {
+                extractor: Box::new(move |s| {
+                    s.accept_seq(splitter.as_ref().chars()).then_some(s.view())
+                }),
+                discard: true,
             });
         }
-        self.splitters
-            .sort_by_key(|b| std::cmp::Reverse(b.text.len()));
         self
     }
 
@@ -98,87 +170,82 @@ impl<I: Iterator<Item = char>> StringTokenizer<I> {
     }
 }
 
-impl<I: Iterator<Item = char>> Iterator for StringTokenizer<I> {
+impl<'a, I: Iterator<Item = char>> Iterator for StringTokenizer<'a, I> {
     type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
+            if let Some(token) = self.queued_token.take() {
+                return Some(token);
+            }
+
+            let sofar = self.src.matched_len();
+
             // Skip any chars we don't care about (eg: whitespace)
             if !self.src.accept_while(self.trimmer).is_empty() {
-                let _ = self.src.lift(); // drop it
+                if sofar > 0 {
+                    let ret = Some(self.src.lift_first(sofar).collect());
+                    let _ = self.src.lift(); // drop the trimmer chars
+                    return ret;
+                }
+                let _ = self.src.lift(); // drop the trimmer chars
             }
 
-            // Check if we hit an escape pair region
-            if let Some(pair) = self
-                .escape_pairs
-                .iter()
-                .find(|p| self.src.accept_seq(p.start.chars()))
+            // Check extractors (TODO: consolidate with splitters / escape pairs)
+            if let Some(extractor) = self
+                .extractors
+                .iter_mut()
+                .find(|e| (e.extractor)(&mut self.src).is_some())
             {
-                // consume all escaped content until end delimiter.
-                while let Some(c) = self.src.peek() {
-                    if Some(*c) == pair.escape_char {
-                        self.src.advance(); // consume escape char
-                        self.src.advance(); // consume escaped char
-                        continue;
+                if sofar > 0 {
+                    let ret = Some(self.src.lift_first(sofar).collect());
+                    let next = self.src.lift();
+                    if !extractor.discard {
+                        self.queued_token = Some(next.collect());
                     }
-                    if self.src.accept_seq(pair.end.chars()) {
-                        break;
-                    }
-                    self.src.advance();
+                    return ret;
                 }
-                // strip delimiters if needed
-                let result: String = self.src.lift().collect();
-                if !pair.keep_delimiters {
-                    let mut s = result.as_str();
-                    s = s.strip_prefix(&pair.start).unwrap_or(s);
-                    s = s.strip_suffix(&pair.end).unwrap_or(s);
-                    return Some(s.to_string());
+
+                let ret = self.src.lift();
+                if !extractor.discard {
+                    return Some(ret.collect());
                 }
-                return Some(result);
             }
 
-            // Check if there's a splitter to return at current position.
-            if let Some(splitter) = self
-                .splitters
-                .iter()
-                .find(|s| self.src.accept_seq(s.text.chars()))
+            if let Some((_extractor, discard)) = self
+                .extractors2
+                .iter_mut()
+                .find(|e| e.0.extract(&mut self.src).is_some())
             {
-                // It's a delimiter, skip and look for the next token
-                if splitter.is_delimiter {
-                    let _ = self.src.lift(); // drop it
-                    continue;
-                } else {
-                    return Some(self.src.lift().collect());
+                if sofar > 0 {
+                    let ret = Some(self.src.lift_first(sofar).collect());
+                    let next = self.src.lift();
+                    if !*discard {
+                        self.queued_token = Some(next.collect());
+                    }
+                    return ret;
+                }
+
+                let ret = self.src.lift();
+                if !*discard {
+                    return Some(ret.collect());
                 }
             }
-            // Advance as long as it's not a trimmer char AND we haven't hit a splitter.
-            let mut token_len = 0;
-            while let Some(not_eof_char) = self.src.peek() {
-                // loop til EOF
-                if (self.trimmer)(not_eof_char) {
-                    break; // hit a trimmer
+
+            if self.src.advance().is_none() {
+                if sofar > 0 {
+                    return Some(self.src.lift_first(sofar).collect());
                 }
-                if self
-                    .splitters
-                    .iter()
-                    .any(|s| self.src.peek_seq(s.text.chars()))
-                {
-                    break; // hit a splitter
-                }
-                token_len += 1;
-                self.src.advance();
+                return None; // EOF
             }
-            if token_len > 0 {
-                return Some(self.src.lift().collect());
-            }
-            return None;
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{EscapePair, StringTokenizer};
+    use super::StringTokenizer;
+    use crate::string_tokenizer::*;
 
     #[test]
     fn split_on_trimmer() {
@@ -209,53 +276,71 @@ mod tests {
     #[test]
     fn split_on_symbols() {
         // single-char symbols
-        let lx = StringTokenizer::from("1+2*3/5").symbols(["/", "+", "*"]);
+        // let lx = StringTokenizer::from("1+2*3/5").symbols(["/", "+", "*"]);
+        let lx = StringTokenizer::from("1+2*3/5").split_on(symbols(["/", "+", "*"]), false);
         assert_eq!(lx.collect::<Vec<_>>(), ["1", "+", "2", "*", "3", "/", "5"]);
+
+        let lx = StringTokenizer::from("1+2*3/5").split_on2(["/", "+", "*"], false);
+        assert_eq!(lx.collect::<Vec<_>>(), ["1", "+", "2", "*", "3", "/", "5"]);
+
         // mixed-length symbols
-        let lx = StringTokenizer::from("a:=3 b:4 c=5").symbols([":=", ":"]);
+        // let lx = StringTokenizer::from("a:=3 b:4 c=5").symbols([":=", ":"]);
+        let lx = StringTokenizer::from("a:=3 b:4 c=5").split_on(symbols([":=", ":"]), false);
         assert_eq!(
             lx.collect::<Vec<_>>(),
             ["a", ":=", "3", "b", ":", "4", "c=5"]
         );
         // symbols and delimiters
         let lx = StringTokenizer::from("a:=3,b:4")
-            .symbols([":=", ":"])
+            .split_on(symbols([":=", ":"]), false)
             .delimiters([","]);
         assert_eq!(lx.collect::<Vec<_>>(), ["a", ":=", "3", "b", ":", "4"]);
+
+        let lx = StringTokenizer::from(",b:4")
+            .split_on(symbols([":=", ":"]), false)
+            .delimiters([","]);
+        assert_eq!(lx.collect::<Vec<_>>(), ["b", ":", "4"]);
     }
 
     #[test]
     fn escape_pairs() {
         // Keep quotes
         let lx = StringTokenizer::from(r#"hello "escaped \" string" world"#)
-            .escape_pairs([EscapePair::new("\"", "\"")]);
+            .split_on(quoted("\"", "\"", Some('\\')), false);
+        assert_eq!(
+            lx.collect::<Vec<_>>(),
+            ["hello", r#""escaped \" string""#, "world"]
+        );
+
+        let lx = StringTokenizer::from(r#"hello "escaped \" string" world"#)
+            .split_on(|s| extract_quoted(s, "\"", "\"", Some('\\')), false);
         assert_eq!(
             lx.collect::<Vec<_>>(),
             ["hello", r#""escaped \" string""#, "world"]
         );
 
         // Drop quotes
-        let lx = StringTokenizer::from(r#"hello "escaped \" string" world"#)
-            .escape_pairs([EscapePair::new("\"", "\"").drop_delimiters()]);
-        assert_eq!(
-            lx.collect::<Vec<_>>(),
-            ["hello", r#"escaped \" string"#, "world"]
-        );
-
-        // Custom block comment with no escape char, dropping the bounds
-        let lx = StringTokenizer::from("code /* some \n comment */ more code").escape_pairs([
-            EscapePair::new("/*", "*/")
-                .escape_char(None)
-                .drop_delimiters(),
-        ]);
-        assert_eq!(
-            lx.collect::<Vec<_>>(),
-            ["code", " some \n comment ", "more", "code"]
-        );
-
-        // Unterminated quote
-        let lx = StringTokenizer::from(r#"start "unterminated"#)
-            .escape_pairs([EscapePair::new("\"", "\"")]);
-        assert_eq!(lx.collect::<Vec<_>>(), ["start", r#""unterminated"#]);
+        // let lx = StringTokenizer::from(r#"hello "escaped \" string" world"#)
+        //     .escape_pairs([EscapePair::new("\"", "\"").drop_delimiters()]);
+        // assert_eq!(
+        //     lx.collect::<Vec<_>>(),
+        //     ["hello", r#"escaped \" string"#, "world"]
+        // );
+        //
+        // // Custom block comment with no escape char, dropping the bounds
+        // let lx = StringTokenizer::from("code /* some \n comment */ more code").escape_pairs([
+        //     EscapePair::new("/*", "*/")
+        //         .escape_char(None)
+        //         .drop_delimiters(),
+        // ]);
+        // assert_eq!(
+        //     lx.collect::<Vec<_>>(),
+        //     ["code", " some \n comment ", "more", "code"]
+        // );
+        //
+        // // Unterminated quote
+        // let lx = StringTokenizer::from(r#"start "unterminated"#)
+        //     .escape_pairs([EscapePair::new("\"", "\"")]);
+        // assert_eq!(lx.collect::<Vec<_>>(), ["start", r#""unterminated"#]);
     }
 }
